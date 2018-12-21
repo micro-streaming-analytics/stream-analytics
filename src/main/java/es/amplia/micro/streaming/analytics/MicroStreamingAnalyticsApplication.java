b18c84e1816b4617f7052fdf4eaa07ac62db2693
Ricardo Daza <ricardo.daza@outlook.com>
2018-12-21 09:28:24 +0100

    Create pom.xml and packages

---
+++ src/main/java/es/amplia/micro/streaming/analytics/MicroStreamingAnalyticsApplication.java
@@ -0,0 +1,14 @@
+package es.amplia.micro.streaming.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroStreamingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroStreamingAnalyticsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/es/amplia/micro/streaming/analytics/MicroStreamingAnalyticsApplicationTests.java
@@ -0,0 +1,17 @@
+package es.amplia.micro.streaming.analytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroStreamingAnalyticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
