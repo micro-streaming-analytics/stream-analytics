7c4ac2e57b15b071cd34c21e903a7875729b4203
Ricardo Daza <ricardo.daza@outlook.com>
2018-12-26 22:17:33 +0100

    RabbitMQ listener working fine

--- src/main/java/es/amplia/micro/streaming/analytics/services/RabbitMessageListener.java
+++ src/main/java/es/amplia/micro/streaming/analytics/services/RabbitMessageListener.java
@@ -1,8 +1,11 @@
 package es.amplia.micro.streaming.analytics.services;
 
-import es.amplia.micro.streaming.analytics.dto.DMMCollectionDto;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 public interface RabbitMessageListener {
 
-	void listenMessages(final DMMCollectionDto collection);
+	void listenMessages(final String collection) throws JsonParseException, JsonMappingException, IOException;
 }
