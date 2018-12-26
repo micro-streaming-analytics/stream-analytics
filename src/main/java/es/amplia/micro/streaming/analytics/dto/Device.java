538a6bf633cea1c3557f678f0956b9b79104773b
Ricardo Daza <ricardo.daza@outlook.com>
2018-12-26 23:02:12 +0100

    Change database collection for deviceDto

--- src/main/java/es/amplia/micro/streaming/analytics/dto/Device.java
+++ src/main/java/es/amplia/micro/streaming/analytics/dto/Device.java
@@ -6,7 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="dmmCollectionDto")
+@Document(collection="deviceDto")
 public class Device {
 
 	@Id
